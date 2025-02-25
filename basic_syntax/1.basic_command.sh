caa0e467096f7f6960857db9d9e4c0d626a53b29
jshjshjshjsh <cj456456@gmail.com>
2025-02-25 21:46:50 +0900

    fix

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -77,3 +77,14 @@ git stash list
 
 # 저장목록 모두 삭제
 git stash clear
+
+# 버전명시
+git tag v1.0
+git tag -a v1.0 -m "1.주요사항A 2.주요사항B"
+
+# tag release 배포
+# commit push와 상관없이 태그별도로 push
+git push origin v1.0
+
+# 태그 목록 조회
+git tag
